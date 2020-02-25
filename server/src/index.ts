52e5d3ee7eaee3c5f9f9b97267185b1214e14008
dmitriy365377 <tigr365377@gmail.com>
2020-02-25 17:16:23 +0300

    create get and post express routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,36 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import { listings } from './listing';
+
+
 const app = express();
 const port = 9000;
+
+app.use(bodyParser.json())
 
 const one: number = 1;
 const two: number = 2;
 
 
 app.get("/", (req, res) => res.send(`1 + 2 = ${one + two}`));
+app.get('/listings', (_req, res) => {
+    return res.send(listings)
+})
+
+
+app.post("/delete-listing", (req, res) => {
+    const id: string = req.body.id;
+
+    for (let i = 0; i < listings.length; i++) {
+        if (listings[i].id === id) {
+            return res.send(listings.splice(i, 1));
+        }
+    }
+
+    return res.send("failed to delete listing")
+})
+
+
 app.listen(port);
 
 console.log(`[app]: http://localhost:${port}`);

51dba172d8987eb9274c64c2fe5f4553703a1d7e
dmitriy365377 <tigr365377@gmail.com>
2020-02-29 20:13:53 +0300

    add env file and gitignore for password

--- server/src/database/indes.ts
+++ server/src/database/indes.ts
@@ -5,7 +5,7 @@ const user = 'user_31'
 const userPassword = 'DJFQWc6W2jaigCDc'
 const cluster = 'cluster0-goeb1'
 
-const url = `mongodb+srv://${user}:${userPassword}@${cluster}.mongodb.net/test?retryWrites=true&w=majority`
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/test?retryWrites=true&w=majority`
 
 export const connectDatabase = async (): Promise<Database> => {
     const client = await MongoClient.connect(url, {
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,19 @@
+require('dotenv').config();
+
+
 import { connectDatabase } from './database/indes';
 import express, { Application } from 'express'
 import { ApolloServer } from 'apollo-server-express'
 import { typeDefs } from './graphQL/typeDefs';
 import { resolvers } from './graphQL/resolvers'
-
-
-const port = 9000;
-const one: number = 1;
-const two: number = 2;
-
-// app.get("/", (req, res) => res.send(`1 + 2 = ${one + two}`));
-
+ 
 const mount = async (app: Application) => {
     const db = await connectDatabase()
     const server = new ApolloServer({ typeDefs, resolvers, context: () => ({ db }) });
     server.applyMiddleware({ app, path: '/api' });
 
-    app.listen(port);
-    console.log(`[app]: http://localhost:${port}`);
+    app.listen(process.env.PORT);
+    console.log(`[app]: http://localhost:${process.env.PORT}`);
 
     const listings = await db.listings.find({}).toArray();
     console.log(listings)
